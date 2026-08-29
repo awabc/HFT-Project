978376fe4cb8e9e1777cfd8b43949e8178e2f9df
awabc <awabchoudhury@gmail.com>
2026-08-29 17:28:10 -0400

    add parser to top module, clear up i/o names

--- rtl/hft_top.v
+++ rtl/hft_top.v
@@ -22,32 +22,32 @@ module hft_top #(
 	input                       tx_tready,
 	
 	// Configuration signals, drive through VIO
-	input [15:0]           		udp_port,
-	input [15:0] 				locate,
-	input						enable,
-	input [31:0] 				buy_below_value,
-	input [31:0] 				sell_above_value,
-	input [31:0] 				quantity,
-	input 						arm,
+	input [15:0]           		i_udp_port,
+	input [15:0] 				i_locate,
+	input						i_enable,
+	input [31:0] 				i_buy_below_value,
+	input [31:0] 				i_sell_above_value,
+	input [31:0] 				i_quantity,
+	input 						i_arm,
 	
 	// Status signals
-	output [31:0] 				best_bid,
-	output [31:0] 				best_bid_quantity,
-	output 						best_bid_valid,
-	output [31:0]				best_ask,
-	output [31:0] 				best_ask_quantity,
-	output 						best_ask_valid,
-	output 						armed,
+	output [31:0] 				o_best_bid,
+	output [31:0] 				o_best_bid_quantity,
+	output 						o_best_bid_valid,
+	output [31:0]				o_best_ask,
+	output [31:0] 				o_best_ask_quantity,
+	output 						o_best_ask_valid,
+	output 						o_armed,
 	
 	// Stats
-	output [31:0] 				stat_frames,
-	output [31:0] 				stat_accepted,
-	output [31:0]				stat_bad_fcs,
-	output [31:0] 				stat_dropped,
-	output [31:0] 				stat_fires,
-	output [31:0] 				stat_orders,
-	output 						stat_overrun,
-	output 						stat_book_conflict
+	output [31:0] 				o_stat_frames,
+	output [31:0] 				o_stat_accepted,
+	output [31:0]				o_stat_bad_fcs,
+	output [31:0] 				o_stat_dropped,
+	output [31:0] 				o_stat_fires,
+	output [31:0] 				o_stat_orders,
+	output 						o_stat_overrun,
+	output 						o_stat_book_conflict
 
 );
 
@@ -55,6 +55,46 @@ module hft_top #(
 	///////////////////////////
 	//         Parser        //
 	///////////////////////////
+	
+	wire 			event_valid;
+	wire [144:0] 	event_marker;
+	wire [63:0] 	event_sequence;
+	wire 			rx_hdr_hit;
+	wire 			rx_frame_bad;
+	wire [31:0]		stat_frames;
+	wire [31:0] 	stat_accepted;
+	wire [31:0]		stat_bad_fcs;
+	wire [31:0] 	stat_dropped;
+	
+	parser parser_inst (
+		.i_clk				(clk),
+		.i_rst_n			(rst_n),
+		
+		.i_data				(rx_tdata),
+		.i_keep				(rx_tkeep),
+		.i_data_valid		(rx_tvalid),
+		.i_last				(rx_tlast),
+		.i_error			(rx_tuser_error),
+		
+		.i_udp_port			(i_udp_port),
+		
+		.o_event_valid		(event_valid),
+		.o_event_marker		(event_marker),
+		.o_event_sequence   (event_sequence),
+		
+		.o_rx_hdr_hit		(rx_hdr_hit),
+		.o_rx_frame_bad		(rx_frame_bad),
+		
+		.o_stat_frames		(stat_frames),
+		.o_stat_accepted	(stat_accepted),
+		.o_stat_bad_fcs		(stat_bad_fcs),
+		.o_stat_dropped		(stat_dropped)
+	);
+	
+	assign o_stat_frames 	= stat_frames;
+	assign o_stat_accepted 	= stat_accepted;
+	assign o_stat_bad_fcs 	= stat_bad_fcs;
+	assign o_stat_dropped 	= stat_dropped;
 	
 
 	///////////////////////////
